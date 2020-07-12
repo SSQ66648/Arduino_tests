35b3946e4fb0d7c04aeb68d6cd360eab148f970f
TC <ssq16shu@uea.ac.uk>
2020-07-12 23:04:30 +0100

    Update motorSheild_L293D__test.ino
    v2.1: added successful testing of stepper motor (Mitsumi M35SP-7N)

--- motorSheild_L293D__test/motorSheild_L293D__test.ino
+++ motorSheild_L293D__test/motorSheild_L293D__test.ino
@@ -20,23 +20,38 @@
         + using new library.
         + testing with Mitsumi M35SP-7N.
         + limited difference seen at lower speeds (unloaded)
+    v3.0:
+      Mitsumi M35SP-7N:
+        + unsure of exact voltage rating (6 or 24v)
+        + test successful from L293D supply (5v, max0.6A)
+        + VERY slightly under 360 rotation at 48steps, noticably over at 49
+        + trial and error to find effective RPM. max values found to complete full rotation:
+            single coil   <= 400
+            double coil   <= 400
+            interleave    <= 500
+            microstep     <= 750
   TODO:
     + Test BRAKE capability under load/momentum
     + Add tests for stepper motors (assume replacement/additional libraries may be needed)
-  v.20
+  v2.0
     + Look into Adafruit accelStepper?
     + Find limits of library functions (ie. max speed parameters etc)
+  v2.1
+    + Rewrite step tests to own methods (type passed as argument)
   ----------------------------------------------------------------------------*/
 
 //Header files------------------------------------------------------------------
-//lib for v1.0: possibly better than AFMotor for DC motors (need to test BRAKE function)
+//lib for v1.0: possibly better than AFMotor for DC motors? (need to test BRAKE function)
 #include<MotorDriver.h>
-//lib for v2.0 (stepper motor support)
+//lib for v2.0+ (stepper motor support)
 #include<AFMotor.h>
 
 //Member Variables--------------------------------------------------------------
 //DC motor 1
 AF_DCMotor dcMotor(1);
+//stepper motor
+AF_Stepper stepMotor(48, 2);
+
 //spin up/down speed counter
 uint8_t speedCounter;
 
@@ -46,85 +61,75 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Setup complete");
 
-  //turn on motor (zero speed)
-  dcMotor.setSpeed(200);
+  //turn on dc motor (zero speed)
+  dcMotor.setSpeed(100);
   dcMotor.run(RELEASE);
+
+  //set RPM speed of stepper motor
+  stepMotor.setSpeed(750);
 }
 
 
 //Loop--------------------------------------------------------------------------
 void loop() {
-  //FORWARD TEST--------------------------
+  //DC FORWARD TEST-----------------------
+  Serial.println("DC motor test: forward");
   dcMotor.run(FORWARD);
+  dcSpinupTest();
 
-  //spin up motor incrementally
-  Serial.println("motor 1: forward: spin UP");
-  for (speedCounter = 0; speedCounter < 255; speedCounter++) {
-    dcMotor.setSpeed(speedCounter);
-    printSpeed();
-    delay(50);
-  }
-  Serial.println("spin up complete.");
-  delay(1000);
-
-  //spin down motor decrementally
-  Serial.println("motor 1: forward: spin DOWN");
-  for (speedCounter = 255; speedCounter != 0; speedCounter--) {
-    dcMotor.setSpeed(speedCounter);
-    printSpeed();
-    delay(50);
-  }
-  Serial.println("spin down complete.");
-  delay(1000);
-
-
-  //REVERSE TEST--------------------------
+  //DC BACKWARD TEST----------------------
+  Serial.println("DC motor test: backward");
   dcMotor.run(BACKWARD);
+  dcSpinupTest();
 
-  //spin up motor incrementally
-  Serial.println("motor 1: backward: spin UP");
-  for (speedCounter = 0; speedCounter < 255; speedCounter++) {
-    dcMotor.setSpeed(speedCounter);
-    printSpeed();
-    delay(50);
-  }
-  Serial.println("spin up complete.");
-  delay(1000);
-
-  //spin down motor decrementally
-  Serial.println("motor 1: backward: spin DOWN");
-  for (speedCounter = 255; speedCounter != 0; speedCounter--) {
-    dcMotor.setSpeed(speedCounter);
-    printSpeed();
-    delay(50);
-  }
-  Serial.println("spin down complete.");
-
-  //STOP MOTOR----------------------------
+  //STOP DC MOTOR-------------------------
   Serial.println("releasing motor 1");
   dcMotor.run(RELEASE);
-  delay(3000);
+  delay(1000);
+
+  //STEP *1 COIL--------------------------
+  stepMotor.setSpeed(400);
+  Serial.println("Stepper motor: single coil test: forward");
+  stepMotor.step(48, FORWARD, SINGLE);
+  delay(1000);
+  Serial.println("Stepper motor: single coil test: backward");
+  stepMotor.step(48, BACKWARD, SINGLE);
+  delay(2000);
+
+  //STEP *2 COIL--------------------------
+  stepMotor.setSpeed(400);
+  Serial.println("Stepper motor: double coil test: forward");
+  stepMotor.step(48, FORWARD, DOUBLE);
+  delay(1000);
+  Serial.println("Stepper motor: double coil test: backward");
+  stepMotor.step(48, BACKWARD, DOUBLE);
+  delay(2000);
+
+  //STEP INTERLEAVE-----------------------
+  stepMotor.setSpeed(500);
+  //greater accuracy (ie 96 steps ber turn)
+  Serial.println("Stepper motor: interleaved coil test: forward");
+  stepMotor.step(96, FORWARD, INTERLEAVE);
+  delay(1000);
+  Serial.println("Stepper motor: interleaved coil test: backward");
+  stepMotor.step(96, BACKWARD, INTERLEAVE);
+  delay(2000);
+
+  //STEP MICROSTEP------------------------
+  stepMotor.setSpeed(750);
+  Serial.println("Stepper motor: microstep test: forward");
+  stepMotor.step(48, FORWARD, MICROSTEP);
+  delay(1000);
+  Serial.println("Stepper motor: microstep test: backward");
+  stepMotor.step(48, BACKWARD, MICROSTEP);
+  delay(2000);
 
 
+  //STOP PROGRAM--------------------------
+  Serial.println("end loop: press reset button to repeat cycle.");
+  delay(100);
+  exit(0);
 
-  //
-  //  Serial.println("motor 1: ON: 5 seconds");
-  //  motorDriver.motor(1, FORWARD, 255);
-  //  delay(5000);
-  //
-  //  Serial.println("motor 1: BRAKE: 5 seconds");
-  //  motorDriver.motor(1, BRAKE, 255);
-  //  delay(5000);
-  //
-  //  Serial.println("motor 1: BACKWARD: 5 seconds");
-  //  motorDriver.motor(1, BACKWARD, 255);
-  //  delay(5000);
-  //
-  //  Serial.println("motor 1: RELEASE: 5 seconds");
-  //  motorDriver.motor(1, RELEASE, 255);
-  //  delay(5000);
-  //
-  //  ceaseAll();
 }
 
 
@@ -142,6 +147,29 @@ void printSpeed() {
   }
 }
 
+void dcSpinupTest() {
+  //spin up motor incrementally
+  Serial.println("motor 1: spin UP");
+  for (speedCounter = 0; speedCounter < 255; speedCounter++) {
+    dcMotor.setSpeed(speedCounter);
+    printSpeed();
+    delay(50);
+  }
+  Serial.println("spin up complete.");
+  delay(1000);
+
+  //spin down motor decrementally
+  Serial.println("motor 1: spin DOWN");
+  for (speedCounter = 255; speedCounter != 0; speedCounter--) {
+    dcMotor.setSpeed(speedCounter);
+    printSpeed();
+    delay(50);
+  }
+  Serial.println("spin down complete.");
+  delay(1000);
+}
+
+//obsolete (todo: rewrite for current library)
 //--------------------------------------
 // release all motors before ceasing all activity indefinitely
 //--------------------------------------
